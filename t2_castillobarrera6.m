61b7763dd4005ef1d33a71d0c00781ea71395ba0
nadiacastillo <43325393+nadiacastillo@users.noreply.github.com>
2018-10-03 19:45:55 -0500

    Add files via upload

---
+++ t2_castillobarrera6.m
@@ -0,0 +1,12 @@
+prompt=('litros consumidos')
+n=input(prompt)
+if n<=50
+    if n>50
+        if n<200
+            ((150)+(n-50)*3)
+        end
+    end
+    if n>200
+        (600)+(n-200)*5
+    end
+end
